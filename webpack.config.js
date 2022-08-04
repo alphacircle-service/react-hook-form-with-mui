db27bd87d4ecc3abd77cd58032456fd73709ae8e
june <june@JUNE.localdomain>
2022-08-04 11:25:51 +0900

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,53 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-const webpack = require('webpack');
-const prod = process.env.NODE_ENV === 'production';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const webpack = require('webpack')
+
+const prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
-    mode: prod ? 'production' : 'development',
-    devtool: prod ? 'hidden-source-map' : 'eval',
-    entry: './src/index.tsx',
-    
-    resolve : {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        modules: [
-            path.join(__dirname, "src"),
-            "node_modules"
-          ]
-    },
+  mode: prod ? 'production' : 'development',
+  devtool: prod ? 'hidden-source-map' : 'eval',
+  entry: './src/index.tsx',
 
-    module : {
-        rules : [
-            {
-                test: /\.tsx?$/,
-                use: ['babel-loader', 'ts-loader']
-            },
-        ]
-    },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    modules: [path.join(__dirname, 'src'), 'node_modules'],
+  },
 
-    output : {
-        path: path.join(__dirname, '/dist'),
-        filename: 'bundle.js'
-    },
-
-    plugins:[
-        new webpack.ProvidePlugin({
-            React: 'react',
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new webpack.HotModuleReplacementPlugin(),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['babel-loader', 'ts-loader'],
+      },
     ],
+  },
 
-    devServer : {
-        historyApiFallback: true,
-        port : 3000,
-        hot: true,
-        compress:true,
-        static: {
-            directory: path.join(__dirname, 'public')
-        }
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      React: 'react',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+
+  devServer: {
+    historyApiFallback: true,
+    port: 3000,
+    hot: true,
+    compress: true,
+    static: {
+      directory: path.join(__dirname, 'public'),
     },
-
+  },
 }
